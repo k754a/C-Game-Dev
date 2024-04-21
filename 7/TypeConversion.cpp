6dda88ff882e0d43927d84014204a868b53c4084
k754a <114301997+k754a@users.noreply.github.com>
2024-04-21 10:11:07 -0600

    Type Conversions

---
+++ 7/TypeConversion.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+int main()
+{
+    //converts lets say an int to a string, we can do it automatilcy or manualy
+
+    //lets do it automatilcy first
+
+    //its like unity              var = ("the type to change")"the number"
+
+    double i = (int)3.45;
+
+    //lets convert 100 to a character
+    std::cout << (char)100 << std::endl;
+
+
+    //lets make a basic score algorithem
+
+    int correct = 20;
+    int questions = 25;
+
+     std::cout << (double)correct/questions * 100 << "%" << std::endl;
+
+    
+
+    
+    
+    return 0;
+}
