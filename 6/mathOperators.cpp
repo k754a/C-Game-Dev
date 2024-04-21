f9ca5f681cdfe46a9d4d709cfcbe7a1811b2e755
k754a <114301997+k754a@users.noreply.github.com>
2024-04-21 10:00:49 -0600

    6

--- 6/mathOperators.cpp
+++ 6/mathOperators.cpp
@@ -5,6 +5,9 @@ int main()
     //arithmetic operators = return the result of - + * /
 
     int apples = 20; 
+
+
+    //double apples = 20;
     //lets say i eat 10 apples
 
     apples = apples - 10;
@@ -16,6 +19,13 @@ int main()
 
     //in loops we can use this
     apples++;
+    //or this
+    apples--;
+
+    //we cannot do 
+    //apples**;
+    //or
+    //apples//;
 
     //lets say i double my apples i can do this
 
@@ -28,12 +38,25 @@ int main()
     //we can also do this
 
     apples*=2;
+
+    apples/=11;
     
-    apples/=2;
+
+    //we need to remeber becase we are using a int, we cannot get decimals, it will just remove them.
+
+    //we can use a double to fix that though.
+
+    //we can use a remainder:
+    int remainder = apples % 3; //only works with ints i think
+
+    //basicly we want to see if we can devide evenly and see what the remander is, so 13 /2 would have a remainder of 1 while 12/2 would be a remainder of 0, its good to use if you need to see if something is even or odd.
+    //it uses bedmas: brackets and * and / then + and -
+
     
 
 
-    std::cout << apples <<std::endl;
+
+    std::cout << remainder <<std::endl;
 
 
 
